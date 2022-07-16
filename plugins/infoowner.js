8d598778959a97bb312af1ac03024b8069ef49b1
RynZzz-404 <108714745+RynZzz-404@users.noreply.github.com>
2022-07-16 21:05:45 +0800

    Add files via upload

---
+++ plugins/infoowner.js
@@ -0,0 +1,20 @@
+let handler  = async (m, { conn, usedPrefix }) => { 
+conn.reply(m.chat, `
+╭─˗ˏˋ *INFORMASI ʏᴀɴɴ* ´ˎ˗
+│ ✎ _Nama_ : 𝓜.𝓡𝔂𝓪𝓷.𝓐 𝓢.𝓢𝓲
+│ ✎ _Umur_ : 23🗿
+│ ✎ _Asal_ : ʙᴀɴᴅᴜɴɢ,ᴊᴀᴡᴀ ʙᴀʀᴀᴛ
+│ ✎ _Status_ : ɴɢᴀᴊᴀʀ🗿
+│ ✎ _WhatsApp_ :
+│    wa.me/6282252285143
+╰──────────────
+`.trim(), m)
+}
+
+handler.help = ['infoowner']
+handler.tags = ['main', 'utama']
+handler.command = /^(infoalpi)$/i
+
+handler.exp = 150
+
+module.exports = handler
