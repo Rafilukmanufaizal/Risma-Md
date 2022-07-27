d3cda6131c84ae24dc91c1e277539701452cf5a9
Rafilukmanufaizal <109738595+Rafilukmanufaizal@users.noreply.github.com>
2022-07-27 18:29:30 +0700

    infoowner.js

--- plugins/infoowner.js
+++ plugins/infoowner.js
@@ -1,12 +1,12 @@
 let handler  = async (m, { conn, usedPrefix }) => { 
 conn.reply(m.chat, `
 ╭─˗ˏˋ *INFORMASI ʏᴀɴɴ* ´ˎ˗
-│ ✎ _Nama_ : 𝓜.𝓡𝔂𝓪𝓷.𝓐 𝓢.𝓢𝓲
-│ ✎ _Umur_ : 23🗿
-│ ✎ _Asal_ : ʙᴀɴᴅᴜɴɢ,ᴊᴀᴡᴀ ʙᴀʀᴀᴛ
-│ ✎ _Status_ : ɴɢᴀᴊᴀʀ🗿
+│ ✎ _Nama_ : 𝓜.𝓡𝓪𝓯𝓲 𝓵𝓾𝓴𝓶𝓪𝓷𝓾 𝓯𝓪𝓲𝔃𝓪𝓵
+│ ✎ _Umur_ : 14
+│ ✎ _Asal_ : ᴋᴇᴅɪʀɪ,ᴊᴀᴡᴀ ᴛɪᴍᴜʀ
+│ ✎ _Status_ : ᴘᴇʟᴀᴊᴀʀ
 │ ✎ _WhatsApp_ :
-│    wa.me/6282252285143
+│    wa.me/6285600380823
 ╰──────────────
 `.trim(), m)
 }
