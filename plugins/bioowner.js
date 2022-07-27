aec55025f509c0a680f84b35e99a79c63aee891a
Rafilukmanufaizal <109738595+Rafilukmanufaizal@users.noreply.github.com>
2022-07-27 18:31:05 +0700

    bioowner.js

--- plugins/bioowner.js
+++ plugins/bioowner.js
@@ -1,17 +1,17 @@
 let handler  = async (m, { conn, usedPrefix }) => { 
 conn.reply(m.chat, `
 ╭─˗ˏˋ *INFORMASI OWNER* ´ˎ˗
-│ ✎ _Nama_ : ℛ𝓎𝒶𝓃 𝒜𝒹𝒾𝓉𝓎𝒶
-│ ✎ _Sekolah_ : ʟᴜʟᴜs 
-│ ✎ _Umur_ : 23
-│ ✎ _Asal_ : ᴊᴀᴡᴀ ʙᴀʀᴀᴛ
+│ ✎ _Nama_ : 𝓕𝓪𝓲𝓼𝓪𝓵
+│ ✎ _Sekolah_ : sᴍᴘ
+│ ✎ _Umur_ : 14
+│ ✎ _Asal_ : ᴊᴀᴡᴀ ᴛɪᴍᴜʀ
 │ ✎ _Status_ : Turu deck
 │ ✎ _Official Grup 1_ :
 │    https://chat.whatsapp.com/B9cQxA0NWrXHtFclMS5awJ
 │ ✎ _Official Grup 2_ :
 │    https://chat.whatsapp.com/DMqCCHW4Uvb2Kd4Cpus1vC
 │ ✎ _WhatsApp_ :
-│    wa.me/6282252285143
+│    wa.me/6285600380823
 ╰───────────────────
 `.trim(), m)
 }
